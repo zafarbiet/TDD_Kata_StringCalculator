150f51045d8a771c042362a4c344c9f56b90f228
Zafar <zafarbiet@gmail.com>
2019-12-22 02:08:56 +0530

    New Project Add

--- TDDKataStringCalculator.Code/StringCalculator.cs
+++ TDDKataStringCalculator.Code/StringCalculator.cs
@@ -8,5 +8,35 @@ namespace TDDKataStringCalculator.Code
 {
     public class StringCalculator
     {
+        public int Add(string number)
+        {
+            if (string.IsNullOrEmpty(number))
+                return 0;
+            var numberForSum = number.Replace('\n', ',').Split(',');
+            ReturnDelimitedNumbers(ref numberForSum);
+            var numberForAdd = numberForSum.Where(c=>!string.IsNullOrEmpty(c)).Select(int.Parse).ToArray();
+            ValidateNegetiveNumbers(numberForAdd);
+            return numberForAdd.Where(c => c < 1001).Sum();
+        }
+
+        private static void ValidateNegetiveNumbers(int[] numberForSum)
+        {
+            if (numberForSum.Any(c => c < 0))
+                throw new Exception($"negatives not allowed {string.Join(" ", numberForSum.Where(c => c < 0))}");
+        }
+
+        private void ReturnDelimitedNumbers(ref string[] numberForSum)
+        {
+            if (numberForSum[0].StartsWith("//"))
+            {
+                var customDelimiter = numberForSum[0].Remove(0, 2).Distinct();
+                foreach (var _delimiter in customDelimiter)
+                {
+                    numberForSum[1] = numberForSum[1].Replace(_delimiter, ',');
+                }
+                numberForSum = numberForSum[1].Split(',');
+            }
+
+        }
     }
 }
--- TDDKataStringCalculator.Tests/StringCalculatorTests.cs
+++ TDDKataStringCalculator.Tests/StringCalculatorTests.cs
@@ -1,12 +1,37 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using TDDKataStringCalculator.Code;
 
 namespace TDDKataStringCalculator.Tests
 {
+    [TestFixture]
     public class StringCalculatorTests
     {
+        [TestCase(0, "")]
+        [TestCase(1, "1")]
+        [TestCase(3, "1,2")]
+        [TestCase(15, "1,2,3,4,5")]
+        [TestCase(6, "1\n2,3")]
+        [TestCase(3, "//;\n1;2")]
+        [TestCase(2, "1001,2")]
+        [TestCase(6, "//***\n1***2***3")]
+        [TestCase(6, "//*%\n1*2%3")]
+        public void Test_StringAdd_ReturnsNumbers(int expected, string number)
+        {
+            var calculator = new StringCalculator();
+            Assert.AreEqual(expected, calculator.Add(number));
+        }
+        [Test]
+        public void TestStringAdd_ReturnsError()
+        {
+            var number = "//;\n-1;-2";
+            var calculator = new StringCalculator();
+            var exception = Assert.Throws<Exception>(() => calculator.Add(number));
+            Assert.AreEqual("negatives not allowed -1 -2", exception.Message);
+        }
     }
 }
